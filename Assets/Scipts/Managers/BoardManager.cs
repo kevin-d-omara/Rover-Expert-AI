1a5c1b19d92552a5019e2680c17255c41c75d745
Kevin O'Mara <kevin.d.omara@gmail.com>
2017-02-09 07:35:37 -0800

    [+] rover animation, [+] tags/sorting layers

--- Assets/Scipts/Managers/BoardManager.cs
+++ Assets/Scipts/Managers/BoardManager.cs
@@ -6,9 +6,37 @@ namespace RoverExpertAI
 {
 	public class BoardManager : MonoBehaviour
 	{
-        private void InitializeBoard()
+        private SimulationParameters parameters;
+
+        private void CreateEmptyBoard()
         {
 
+        }
+
+        private void InitializeValidMoves()
+        {
+
+        }
+
+        private void PlaceObstacles()
+        {
+
+        }
+
+
+        public void SetupBoard(SimulationParameters parameters)
+        {
+            this.parameters = parameters;
+
+            // Create outer walls and floors.
+            CreateEmptyBoard();
+
+            // Create a list including valid board positions, excludes outer
+            // wall and start and end positions.
+            InitializeValidMoves();
+
+            // Add Rocks, Pits, etc. to the board.
+            PlaceObstacles();
         }
     }
 }
